b35557fc9585755efd5d590f49e7278cd37b79ee
matm <mathias@monnerville.com>
2014-10-02 23:17:04 +0200

    Almost working

--- main.go
+++ main.go
@@ -6,33 +6,37 @@ import (
 	"strings"
 )
 
-var alphabet = []string{
-	"A", "B", "C", "D", "E", "F", "G", "H",
-	"I", "J", "K", "L", "M", "N", "O", "P",
-	"Q", "R", "S", "T", "U", "V", "W", "X",
-	"Y", "Z",
-}
-
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Printf("Usage: %s key plain\n", os.Args[0])
 		os.Exit(2)
 	}
-	vgtable := make([][]string, 26)
-	key := strings.ToUpper(os.Args[1])
+	password := strings.ToUpper(os.Args[1])
 	clear := strings.ToUpper(os.Args[2])
-	fmt.Println("CLEAR", clear)
-	key := make([]string, len(clear))
-	for k := 0; k < len(clear); k++ {
-	}
-	if len(key) < len(clear) {
-	}
-	fmt.Println("KEY  ", key)
-	for k := 0; k < 26; k++ {
-		vgtable[k] = make([]string, 26)
-		for j := 0; j < 26; j++ {
-			vgtable[k][j] = alphabet[(j+k)%26]
+	fmt.Println("CLEAR ", clear)
+
+	key := make([]byte, len(clear))
+	if len(password) < len(clear) {
+		j := 0
+		for k := 0; k < len(clear); k++ {
+			key[k] = password[j]
+			j += 1
+			if j > len(password)-1 {
+				j = 0
+			}
 		}
 	}
-	fmt.Println("CIPHER", cipher)
+	fmt.Println("KEY   ", string(key))
+
+	cipher := make([]byte, len(clear))
+	for k := 0; k < len(clear); k++ {
+		cipher[k] = 65 + (clear[k]+key[k])%26
+	}
+	fmt.Println("CIPHER", string(cipher))
+
+	too := make([]byte, len(clear))
+	for k := 0; k < len(clear); k++ {
+		too[k] = 65 + (cipher[k]-key[k])%26
+	}
+	fmt.Println("CLEAR ", string(too))
 }
