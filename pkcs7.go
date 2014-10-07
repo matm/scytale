d0c8e96619ac746e5095ed40bc7940293c1f1333
matm <mathias@monnerville.com>
2014-10-07 09:08:51 +0200

    Add some PKCS#7 unit tests

--- pkcs7.go
+++ pkcs7.go
@@ -2,9 +2,6 @@ package secret
 
 // PKCS#7 data padding
 func PKCS7Padding(clearTextLen, blockSize int) []byte {
-	if clearTextLen < blockSize {
-		return nil
-	}
 	numBytes := blockSize - (clearTextLen % blockSize)
 	padding := make([]byte, numBytes)
 	for j := 0; j < numBytes; j++ {
---
+++ pkcs7_test.go
@@ -0,0 +1,30 @@
+package secret
+
+import (
+	"crypto/aes"
+	"crypto/des"
+	"testing"
+)
+
+func TestPKCS7Padding(t *testing.T) {
+	ins := []struct {
+		length, blockSize int
+		expectLen         byte
+	}{
+		{4227, aes.BlockSize, 13},
+		{1612475, aes.BlockSize, 5},
+		{272473, des.BlockSize, 7},
+		{10, aes.BlockSize, 6},
+	}
+	for _, c := range ins {
+		padding := PKCS7Padding(c.length, c.blockSize)
+		if byte(len(padding)) != c.expectLen {
+			t.Errorf("bad padding length. Expect %d, got %d", c.expectLen, len(padding))
+		}
+		for k := range padding {
+			if padding[k] != c.expectLen {
+				t.Errorf("at pos %d, expect byte %v, got %v", k, c.expectLen, padding[k])
+			}
+		}
+	}
+}
