3a34fa7e834be339e5ced8245093cc7681193dc6
matm <mathias@monnerville.com>
2014-10-08 09:46:25 +0200

    Code refactoring for AES encryption

--- aes.go
+++ aes.go
@@ -6,17 +6,22 @@ import (
 	"crypto/rand"
 	"crypto/sha1"
 	"fmt"
+	"io"
 
 	"code.google.com/p/go.crypto/pbkdf2"
 )
 
 type AES struct {
 	salt, key []byte
+	block     cipher.Block
+	mode      cipher.BlockMode
+	iv        []byte
 }
 
 // AES encryption
 func NewAES(password string) (*AES, error) {
 	passwd := []byte(password)
+	// TODO: random salt
 	salt := []byte{1, 2, 3, 4, 5, 6, 7, 8}
 	key := pbkdf2.Key(passwd, salt, 4096, 32, sha1.New)
 	fmt.Printf("KEY  : %x [%d]\n", key, len(key))
@@ -26,21 +31,48 @@ func NewAES(password string) (*AES, error) {
 		return nil, err
 	}
 
-	return new(AES), nil
+	aes := &AES{salt: salt, key: key, block: block}
+
+	return aes, nil
+}
+
+// Computes a random IV and set cipher's operation mode to CBC.
+func (a *AES) InitEncryption() ([]byte, error) {
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	a.iv = iv
+	a.mode = cipher.NewCBCEncrypter(a.block, iv)
+	return iv, nil
 }
 
 func (a *AES) Encrypt(plaintext []byte) []byte {
-	plain := make([]byte, 0)
+	plain := plaintext
 	if len(plaintext)%aes.BlockSize != 0 {
-		padding = secret.PKCS7Padding(len(plaintext), aes.BlockSize)
-		plain := make([]byte, len(plaintext)+len(padding))
+		padding := PKCS7Padding(len(plaintext), aes.BlockSize)
+		plain = make([]byte, len(plaintext)+len(padding))
 		copy(plain, plaintext)
 		copy(plain[len(plaintext):], padding)
 	}
-	cipher := make([]byte, aes.BlockSize+len(plain))
-	return cipher
+	ciphertext := make([]byte, len(plain))
+	a.mode.CryptBlocks(ciphertext, plain)
+	return ciphertext
+}
+
+// Uses IV and set cipher's operation mode to CBC.
+func (a *AES) InitDecryption(iv []byte) {
+	a.iv = iv
+	a.mode = cipher.NewCBCDecrypter(a.block, iv)
 }
 
 func (a *AES) Decrypt(ciphertext []byte) []byte {
-	return nil
+	a.mode.CryptBlocks(ciphertext, ciphertext)
+	return ciphertext
+}
+
+func (a *AES) RemovePadding(clear []byte) []byte {
+	cnt := clear[len(clear)-1]
+	clear = clear[:len(clear)-int(cnt)]
+	return clear
 }
--- bin/aesfile/main.go
+++ bin/aesfile/main.go
@@ -1,56 +1,30 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"crypto/sha1"
 	"fmt"
-	"io"
 	"log"
 
-	"code.google.com/p/go.crypto/pbkdf2"
 	"secret"
 )
 
 func main() {
-	passwd := []byte("mypasswd")
-	salt := []byte{1, 2, 3, 4, 5, 6, 7, 8}
-	key := pbkdf2.Key(passwd, salt, 4096, 32, sha1.New)
-	fmt.Printf("KEY  : %x [%d]\n", key, len(key))
-
+	a, err := secret.NewAES("mypasswd")
 	plaintext := []byte("This is a crazy message, man")
-
-	padding := secret.PKCS7Padding(len(plaintext), aes.BlockSize)
-	plain := make([]byte, len(plaintext)+len(padding))
-	copy(plain, plaintext)
-	copy(plain[len(plaintext):], padding)
-
-	block, err := aes.NewCipher(key)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	ciphertext := make([]byte, aes.BlockSize+len(plain))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+	// Encrypt
+	iv, err := a.InitEncryption()
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Operation mode
-	mode := cipher.NewCBCEncrypter(block, iv)
-	mode.CryptBlocks(ciphertext[aes.BlockSize:], plain)
-
 	fmt.Printf("PLAIN: %v [%d]\n", string(plaintext), len(plaintext))
+	ciphertext := a.Encrypt(plaintext)
 	fmt.Printf("CRYPT: %x [%d]\n", ciphertext, len(ciphertext))
 
 	// Decrypt
-	mode = cipher.NewCBCDecrypter(block, ciphertext[:aes.BlockSize])
-	ciphertext = ciphertext[aes.BlockSize:]
-	mode.CryptBlocks(ciphertext, ciphertext)
-
-	// Remove padding
-	cnt := ciphertext[len(ciphertext)-1]
-	ciphertext = ciphertext[:len(ciphertext)-int(cnt)]
-	fmt.Printf("PLAIN: %v [%d]\n", string(ciphertext), len(ciphertext))
+	a.InitDecryption(iv)
+	clear := a.Decrypt(ciphertext)
+	clear = a.RemovePadding(clear)
+	fmt.Printf("PLAIN: %v [%d]\n", string(clear), len(clear))
 }
